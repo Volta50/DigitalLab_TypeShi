fc8a3b3861197b4f03bc442b0dad112cbb0b04cb
chopsuey <dbarraganm@unal.edu.co>
2025-10-11 11:31:02 -0500

    Finalizado: Descripción de hardware del sumador/restador.

--- lab-02/sumador/Restador_4bit.v
+++ lab-02/sumador/Restador_4bit.v
@@ -1,7 +1,7 @@
 module sumador_Restador4bit(
     input [3:0] A, B, //2 vectores de 4 
     input Sel,           // 0 = suma, 1 = resta
-    output [3:0] sum,
+    output [3:0] s0,
     output Co
 );
 
@@ -15,9 +15,9 @@ module sumador_Restador4bit(
     assign Bx[3] = xor(B[3], Sel);
 
     // cadena de sumaodres de 1 bit
-    sumadorOneBit sum_n0 (A[0], Bx[0], Sel, Cout[0], sum[0]);
-    sumadorOneBit sum_n1 (A[1], Bx[1], Cout[0], Cout[1], sum[1]);
-    sumadorOneBit sum_n2 (A[2], Bx[2], Cout[1], Cout[2], sum[2]);
-    sumadorOneBit sum_n3 (A[3], Bx[3], Cout[2], Co, sum[3]);
+    sumadorOneBit sum_n0 (A[0], Bx[0], Sel, Cout[0], s0[0]);
+    sumadorOneBit sum_n1 (A[1], Bx[1], Cout[0], Cout[1], s0[1]);
+    sumadorOneBit sum_n2 (A[2], Bx[2], Cout[1], Cout[2], s0[2]);
+    sumadorOneBit sum_n3 (A[3], Bx[3], Cout[2], Co, s0[3]);
 endmodule
 
